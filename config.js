f899501155477d8547cc28efb8e0f401c5626ec4
techsyhub <techsyhub@gmail.com>
2022-09-13 04:27:38 -0700

    port set to 80

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     MONGO_URI:"mongodb+srv://grubmatch:grubmatch@cluster0.agoztuf.mongodb.net/?retryWrites=true&w=majority",
     YELP_KEY:"nBgmgvrCxOqNJ9o1W-COmjIX7V800ykFMR8bkyU_4Z55KbgM2emBX2-iFzRHXwKDDGJTKwo_-HDYkwfH9HUA0Xh3H-HzyHUWE5CFbHRVbg-1bY8zerHxGMqC76rGYnYx",
-    PORT: 80,    
+    PORT: 8080,    
 
 }
