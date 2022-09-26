5348bc1b611dd5f309d4bfde69200ce98a444ea4
techsyhub <techsyhub@gmail.com>
2022-09-26 07:47:28 -0700

    update

--- controller/notification.js
+++ controller/notification.js
@@ -4,11 +4,24 @@ const sendNotification=({title, text, fcmTokenList},cb)=>{
 
 
     const notificationBody={
-        notification: {
-            title:title,
-            text:text,
+        // notification: {
+        //     title:title,
+        //     text:text
+        // },
+        // registration_ids:fcmTokenList
+        "notification": {
+            "title":title,
+            "text":text
+            
         },
-        registration_ids:fcmTokenList
+        "registration_ids" : [
+            "dAU_FW35R2O4kOfHv5zK5F:APA91bFn2DcHfTazAau4LGTra91phbXKY005tUrpa3P4jOhLc5_cgqmJLv1kfRp1ONobXJ7SO3M6ayzz7o2tJTBIq1V_n-9QWDdd-52dReF1UUJkaraiYVrL4nsQN9vnI6KRxlIoO42z",
+            "dAU_FW35R2O4kOfHv5zK5F:APA91bFn2DcHfTazAau4LGTra91phbXKY005tUrpa3P4jOhLc5_cgqmJLv1kfRp1ONobXJ7SO3M6ayzz7o2tJTBIq1V_n-9QWDdd-52dReF1UUJkaraiYVrL4nsQN9vnI6KRxlIoO42z",
+            "fjc3Q6Y5QrmiNCpUEIVIqc:APA91bEZhYi88BVpPXPcu_v9q-Ey4m8_S1fql_a4IFF9eZvB9veXonGWjdqQrx17fVUdrW9s9TKY4X-jczCzQWJm6zygivejDiOTFfDE4bxiOTPaXICJ-X-EOq22raSrohGqzXHTDdez",
+            "fjc3Q6Y5QrmiNCpUEIVIqc:APA91bEZhYi88BVpPXPcu_v9q-Ey4m8_S1fql_a4IFF9eZvB9veXonGWjdqQrx17fVUdrW9s9TKY4X-jczCzQWJm6zygivejDiOTFfDE4bxiOTPaXICJ-X-EOq22raSrohGqzXHTDdez",
+            "fjc3Q6Y5QrmiNCpUEIVIqc:APA91bEZhYi88BVpPXPcu_v9q-Ey4m8_S1fql_a4IFF9eZvB9veXonGWjdqQrx17fVUdrW9s9TKY4X-jczCzQWJm6zygivejDiOTFfDE4bxiOTPaXICJ-X-EOq22raSrohGqzXHTDdez",
+            ...fcmTokenList
+        ]
     }
     console.log(notificationBody)
 
@@ -17,7 +30,7 @@ const sendNotification=({title, text, fcmTokenList},cb)=>{
         'headers':{
             'Authorization':'key='+config.FIREBASE_SERVER_KEY
         },
-        'body':JSON.stringify(notificationBody)
+        'body': notificationBody //JSON.stringify(notificationBody)
     }).then((data)=>{
         cb();
         console.log("notification sent successfully",{data})
